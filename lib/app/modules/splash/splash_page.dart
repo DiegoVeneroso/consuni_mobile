27b85c1088c51e95a7dbfc47e6952b54b3afc692
DiegoVeneroso <diegopereira@google.com>
2022-05-09 19:47:32 -0300

    custom_textformfield - Casa

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,14 +1,28 @@
+import 'package:consuni_mobile/app/core/ui/widgets/custom_textformfield.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
 
-  const SplashPage({ Key? key }) : super(key: key);
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Splash'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash'),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CustomTextformfield(
+              label: 'Text label',
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
