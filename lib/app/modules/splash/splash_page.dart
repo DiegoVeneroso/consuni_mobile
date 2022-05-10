1b63f95cf4a85159860bb07d0d9c11e4c8e262cf
DiegoVeneroso <diegoveneroso.unipampa@gmail.com>
2022-05-10 09:41:48 -0300

    Contrução da splash page

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -2,6 +2,7 @@ import 'package:consuni_mobile/app/core/ui/widgets/custom_appbar.dart';
 import 'package:consuni_mobile/app/core/ui/widgets/custom_buttom.dart';
 import 'package:consuni_mobile/app/core/ui/widgets/custom_textformfield.dart';
 import 'package:flutter/material.dart';
+import 'package:get/utils.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -9,23 +10,43 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppbar(),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: CustomTextformfield(
-              label: 'Text label',
+      body: Container(
+        color: const Color(0xFF140E0E), //cor de fundo da splash
+        child: Stack( // empilha itens na tela
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.width, //adicionar antes o contextextensionss do get para usar o context.width
+                child: Image.asset(
+                  'assets/images/lanche.png',
+                  fit: BoxFit.cover, //ajusta a imagem nas margens do fundo
+                ),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          CustomButtom(label: 'Entrar', onPressed: (){})
-        ],
+            Align(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  SizedBox(
+                     height: context.heightTransformer(reducedBy:85), //usar o context do get para dar margem do topo de 15% do tamanho da tela
+                  ),
+                  Image.asset('assets/images/logo.png'),
+                  const SizedBox(
+                     height: 60,
+                  ),
+                  CustomButtom(
+                    label: 'ACESSAR',
+                    width: context.widthTransformer(reducedBy: 40), //reducedBY reduz o tamanho em 40%
+                    height: 35,
+                    onPressed: (){}
+                    ),
+                ],
+              ),
+            )
+
+          ],
+        ),
       ),
     );
   }
